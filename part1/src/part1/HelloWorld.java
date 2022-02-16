b167912381472aec8ef85ded6605990e2a5c9663
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-16 19:50:10 +0900

    Add files via upload
    주석문

--- part1/src/part1/HelloWorld.java
+++ part1/src/part1/HelloWorld.java
@@ -1,7 +1,12 @@
 package part1;
 
 public class HelloWorld {
+	//프로그램의 시작점.
 	public static void main(String[] args) {
 		System.out.println("Hello World"); 
+		System.out.println("test"); 
+		System.out.println("1"); 
+		System.out.println("2"); 
+		System.out.println("3"); 
 	}
 }
