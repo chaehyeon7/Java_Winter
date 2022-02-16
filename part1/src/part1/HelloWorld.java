006e437ee9bf2152ae764e94eff1dbd9ffc24117
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-02-16 19:56:31 +0900

    주석 추가

--- part1/src/part1/HelloWorld.java
+++ part1/src/part1/HelloWorld.java
@@ -2,11 +2,17 @@ package part1;
 
 public class HelloWorld {
 	//프로그램의 시작점.
+	/*
+	 * 여러줄 주석
+	 * 가
+	 * 능
+	 * 
+	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World"); 
+		/*System.out.println("Hello World"); 
 //		System.out.println("test"); 
 		System.out.println("1"); 
 		System.out.println("2"); 
-		System.out.println("3"); 
+		System.out.println("3"); */
 	}
 }
