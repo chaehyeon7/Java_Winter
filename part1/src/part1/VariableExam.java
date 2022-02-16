942bd282f00fa2edbb9c348e416f5de3f9f2e64a
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-16 20:06:59 +0900

    변수 사용

---
+++ part1/src/part1/VariableExam.java
@@ -0,0 +1,23 @@
+package part1;
+
+public class VariableExam {
+	public static void main(String[] args) {
+		int count;
+		
+		count = 10;
+		
+		count = 20;
+		
+		System.out.println(count);
+		
+//		count = 11.1;  //int는 정수이기에 실수 x
+		
+		double avg = 11.1;
+		
+		String name = "carami";  //변수는 첫문자 항상 소문자
+		
+		int totalCount;
+		
+		
+	}
+}
