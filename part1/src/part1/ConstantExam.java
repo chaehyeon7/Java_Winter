fb35455edff6571b4b31b9489eac694f1e510e9b
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-02-17 18:33:27 +0900

    상수 final

---
+++ part1/src/part1/ConstantExam.java
@@ -0,0 +1,23 @@
+package part1;
+
+public class ConstantExam {
+	public static void main(String[] args) {
+		int i;
+		i = 10;
+		i = 5;
+		
+		final int j;
+		j = 10;
+		
+//		j = 5;
+		
+		double circleArea;
+		final double PI = 3.14159;
+		circleArea = 3 * 3 * PI;
+		
+		final int OIL_PRICE = 1450; 
+		
+		int totalPrice = 50 * OIL_PRICE;
+		
+	}
+}
