309c8401751d287b5bda48aa0d71263395e0922b
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-18 15:28:25 +0900

    조건문, 산술연산자, 우선순위

---
+++ part1/src/part1/PrimitiveDataTypeExam.java
@@ -0,0 +1,19 @@
+package part1;
+
+public class PrimitiveDataTypeExam {
+	public static void main(String[] args) {
+		boolean isFun = false;
+		System.out.println(isFun);
+		
+		char c = 'f';
+		
+		int x = 59;
+		
+		long bing = 23423421123L;
+		
+		float f = 32.4f;
+		
+		double d = 423423423.4;
+		
+	}
+}
---
+++ part1/src/part1/TypeCastingExam.java
@@ -0,0 +1,11 @@
+package part1;
+
+public class TypeCastingExam {
+	public static void main(String[] args) {
+		int x = 50000;
+		long y = x;  //묵시적 형변환
+		
+		long x2 = 5;
+		int y2 = (int)x2;  //강제형변환
+	}
+}
