137ed9409d4b7f2a69b6f7fe97d5ca0cd828a0f8
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-02-18 14:43:24 +0900

    산술 연산자

---
+++ part1/src/part1/OperatorExam.java
@@ -0,0 +1,13 @@
+package part1;
+
+public class OperatorExam {
+	public static void main(String[] args) {
+		int i1 = -5;
+		int i2 = +i1;
+		int i3 = -5;
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+	}
+}
