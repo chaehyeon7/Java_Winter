19e34c1b6551e1871720f5850a44a071e325d6d6
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-02-20 20:20:10 +0900

    if문 추가

--- winter_java/src/part2/OperatorExam5.java
+++ winter_java/src/part2/OperatorExam5.java
@@ -5,5 +5,14 @@ public class OperatorExam5 {
 		int b1 = (5 < 4) ? 50 : 40;
 		
 		System.out.println(b1);
+		
+		int b2 = 0;
+		if(5<4) {
+			b2 = 50;
+		}else {
+			b2 = 40;
+		}
+		
+		System.out.println(b2);
 	}
 }
