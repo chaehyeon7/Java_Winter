08b51c25b9b347072270602a6e77559e154231d1
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-22 19:04:19 +0900

    메소드 오버로딩

---
+++ part5/MethodOverloadExam.java
@@ -0,0 +1,13 @@
+package part5;
+
+public class MethodOverloadExam {
+		public static void main(String args[]){
+            MyClass2 m = new MyClass2();
+            
+            System.out.println(m.plus(5,10));
+            
+            System.out.println(m.plus(5,10,15));
+            
+            System.out.println(m.plus("hello" + " world"));
+        }
+    }
