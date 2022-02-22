39d0f8c60868263d976c5967ae86ea7e8d758252
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-02-22 16:01:49 +0900

    while문

---
+++ winter_java/src/part2/WhileExam.java
@@ -0,0 +1,13 @@
+package part2;
+
+public class WhileExam {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;  // i=i+1;
+		}
+	}
+
+}
