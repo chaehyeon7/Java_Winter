fff8ef153b5370efd958cf4b4eb66161e96a909f
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-22 19:11:20 +0900

    생성자 오버로딩과 this

--- part5/Car.java
+++ part5/Car.java
@@ -1,9 +1,22 @@
 package part5;
 public class Car{
+	//타입 필드명
         String name;
         int number;
 
-        public Car(String n){
-            name = n;
+        public Car(String name){
+            this.name = name;
+        }
+        
+        public Car() {
+//        	this.name = "이름없음";
+//        	this.number = 0;
+        	this("이름없음",0);
+        	
+        }
+        
+        public Car(String name, int number) {
+        	this.name = name;
+        	this.number = number;
         }
     }
