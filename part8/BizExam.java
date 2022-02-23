5115f93653c314cedfd1c55039bef541fbf1447d
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-23 18:11:27 +0900

    사용자 정의 Exception

---
+++ part8/BizExam.java
@@ -0,0 +1,13 @@
+package part8;
+
+public class BizExam {
+	public static void main(String[] args) {
+		BizService biz = new BizService();
+		biz.bizMethod(5);
+		try {
+			biz.bizMethod(-2);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ part8/BizException.java
@@ -0,0 +1,10 @@
+package part8;
+
+public class BizException extends RuntimeException {
+	public BizException(String msg) {
+		super(msg);
+	}
+	public BizException(Exception ex) {
+		super(ex);
+	}
+}
