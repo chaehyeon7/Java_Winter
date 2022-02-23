2f4ff39335d75066061e27c9cc74a9aa1f18d150
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-23 18:03:57 +0900

    Exception발생시키기

---
+++ part8/ExceptionExam3.java
@@ -0,0 +1,22 @@
+package part8;
+
+public class ExceptionExam3 {
+	public static void main(String[] args) {
+        int i = 10;
+        int j = 0;
+        try {
+    	int k = divide(i, j);
+        System.out.println(k);
+        }catch(IllegalAccessException e) {
+    	System.out.println(e.toString());
+	}
+}
+
+    public static int divide(int i, int j) throws IllegalAccessException{
+        if(j==0) {
+        	throw new IllegalAccessException("0으로 나눌 수 없습니다. ");
+        }
+    	int k = i / j;
+        return k;
+    }
+}
