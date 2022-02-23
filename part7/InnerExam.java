6d3f77b8176ff50d32ed9f21f47b0f05846272c1
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-23 17:32:34 +0900

    내부클레스

---
+++ part7/InnerExam.java
@@ -0,0 +1,17 @@
+package part7;
+
+public class InnerExam {
+	class Cal{
+		int value = 0;
+		public void plus() {
+			value++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		InnerExam t = new InnerExam();
+		InnerExam.Cal cal = t.new Cal();
+		cal.plus();
+		System.out.println(cal.value);
+	}
+}
